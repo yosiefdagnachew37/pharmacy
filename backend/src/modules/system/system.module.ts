e292892a7da42c35170beb5ae4d68ffcd016ec5f
yosiefdagnachew <2122232425dd@gmail.com>
2026-03-15 14:33:37 +0300

    GRN number uniqueness

--- backend/src/modules/system/system.module.ts
+++ backend/src/modules/system/system.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SystemService } from './system.service';
 import { SystemController } from './system.controller';
+import { Sale } from '../sales/entities/sale.entity';
+import { Batch } from '../batches/entities/batch.entity';
+import { GoodsReceipt } from '../purchase-orders/entities/goods-receipt.entity';
 
 @Module({
     controllers: [SystemController],
